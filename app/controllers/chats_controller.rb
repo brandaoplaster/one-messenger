a569e633576d6d9d635c9536d51ff99eadf4941f
brandaoplaster <brandaoplaster@gmail.com>
2020-02-02 15:39:07 -0300

    Action index implemeted in the Chats controller to list all users with whom messages have already been exchanged

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,11 @@
 class ChatsController < ApplicationController
+    
+    def index
+        @users = User.joins(:sent).where(messages: { receiver_id: current_user })
+        @user += User.joins(:received).where(messages: { sender_id: current_user })
+        @user.uniq!
+    end
+
     def show
         @user = User.find(params[:id])
     end
