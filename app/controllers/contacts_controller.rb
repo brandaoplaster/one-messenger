7904c43443dda1f5cc7bb5148d72583bf9e773bb
brandaoplaster <brandaoplaster@gmail.com>
2020-02-01 23:21:32 -0300

    Created new and create action along with private contact_params method

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,25 @@ class ContactsController < ApplicationController
     def index
         @contacts = current_user.contacts
     end
+
+    def new
+        @contact = Contact.new
+    end
+
+    def create
+        user = User.find_by(email: contact_params[:record][:email])
+        @contact = current_user.contacts.build(record: user)
+        if @contact.save
+            flash[:notice] = "Contact successfully created"
+            redirect_to contact_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(record: :email)
+    end
 end
