d74ec718062bb6b4c9ff1cf937c2faf376b0fb48
brandaoplaster <brandaoplaster@gmail.com>
2020-02-09 00:16:08 -0300

    Created a route for receiving chat messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :contacts, only: %i(index new create destroy)
-  resources :chats, only: %i(index show)
+  resources :chats, only: %i(index show) do
+    resources :messages, only: :create, defaults: { format: :js }
+  end
 end
